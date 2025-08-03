350eb95076779864a3747371c0324af8e58028fb
Shubham <sbangwal75@gmail.com>
2025-08-03 20:25:48 +0530

    first push

---
+++ src/main/java/com/example/TrendAnalyzerAPI/TrendAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrendAnalyzerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendAnalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendAnalyzerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrendAnalyzerAPI/TrendAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrendAnalyzerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendAnalyzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
